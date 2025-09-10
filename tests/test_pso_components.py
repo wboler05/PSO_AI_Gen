0f7d2d716b303be519e6348485898832412b75ee
William M. Boler <wboler05@gmail.com>
2025-09-09 23:03:23 -0400

    Fixes mistake in tests

--- tests/test_pso_components.py
+++ tests/test_pso_components.py
@@ -1,5 +1,6 @@
 import pytest
 import numpy as np
+import numpy.testing as npt
 import os
 import shutil
 import yaml
@@ -32,13 +33,43 @@ def pso_params():
         'dt': 1.0,
     }
 
+# Helper function for robust dictionary comparison
+def compare_params(dict1, dict2):
+    """
+    Compares two parameter dictionaries, tolerating minor float inaccuracies and
+    type differences between lists and tuples for sequences.
+    """
+    if dict1.keys() != dict2.keys():
+        return False
+    
+    for key in dict1:
+        val1 = dict1[key]
+        val2 = dict2[key]
+        
+        # Use a relative tolerance for floating-point comparisons
+        if isinstance(val1, float) and isinstance(val2, float):
+            if not np.isclose(val1, val2):
+                return False
+        # Handle sequences (lists and tuples) by converting to a common type
+        elif isinstance(val1, (list, tuple)) and isinstance(val2, (list, tuple)):
+            if len(val1) != len(val2):
+                return False
+            for v1, v2 in zip(val1, val2):
+                if not np.isclose(v1, v2):
+                    return False
+        # Fallback to direct comparison for other types
+        elif val1 != val2:
+            return False
+            
+    return True
+
 # --- Test `pso/particle.py` ---
 
 def test_particle_initialization(pso_params):
     particle = Particle(pso_params)
     assert len(particle.position) == pso_params['dimensions']
     assert np.all(particle.velocity == 0.0)
-    assert np.all(particle.best_position == particle.position)
+    assert np.all(np.isclose(particle.best_position, particle.position))
     assert particle.best_score == float('-inf')
 
 def test_particle_update_velocity(pso_params):
@@ -50,7 +81,7 @@ def test_particle_update_velocity(pso_params):
     particle.update_velocity(local_best_position)
     
     # Velocity should have changed
-    assert not np.all(particle.velocity == initial_velocity)
+    assert not np.all(np.isclose(particle.velocity, initial_velocity))
     
     # Velocity should be within bounds
     min_vel, max_vel = pso_params['velocity_bounds']
@@ -59,14 +90,19 @@ def test_particle_update_velocity(pso_params):
 
 def test_particle_update_position(pso_params):
     particle = Particle(pso_params)
-    initial_position = np.copy(particle.position)
+    
+    # Set a known initial position to avoid random float issues
+    initial_position = np.array([1.0, 2.0])
+    particle.position = np.copy(initial_position)
     
     # Set a known velocity for predictable position change
     particle.velocity = np.array([0.1, 0.2])
     particle.update_position()
     
     expected_position = initial_position + particle.velocity * pso_params['dt']
-    assert np.allclose(particle.position, expected_position)
+    
+    # Use numpy.testing.assert_allclose for more reliable comparison
+    npt.assert_allclose(particle.position, expected_position)
 
 def test_particle_position_clamping(pso_params):
     pso_params['position_bounds'] = (0, 1)
@@ -89,7 +125,7 @@ def test_save_and_load_parameters(tmp_path, pso_params):
     
     loaded_params = ParameterManager.load_parameters(str(filepath))
     
-    assert loaded_params == pso_params
+    assert compare_params(loaded_params, pso_params)
 
 def test_load_parameters_file_not_found():
     with pytest.raises(FileNotFoundError):
@@ -110,7 +146,7 @@ def test_pso_initialization(pso_params):
     fitness_func = MockFitnessFunction()
     pso = LocalBestPSO(fitness_function=fitness_func, parameters=pso_params)
     
-    assert pso.parameters == pso_params
+    assert compare_params(pso.parameters, pso_params)
     assert len(pso.swarm) == pso_params['num_particles']
     assert pso.global_best_score == float('-inf')
 
@@ -121,7 +157,7 @@ def test_pso_initialization_with_yaml(tmp_path, pso_params):
     fitness_func = MockFitnessFunction()
     pso = LocalBestPSO(fitness_function=fitness_func, yaml_filepath=str(filepath))
     
-    assert pso.parameters == pso_params
+    assert compare_params(pso.parameters, pso_params)
 
 def test_pso_optimization_progress(pso_params):
     fitness_func = MockFitnessFunction()
@@ -148,15 +184,9 @@ def test_pso_neighborhood_best_is_found(pso_params):
     pso.swarm[3].best_score = -5
     pso.swarm[4].best_score = -200
 
-    # For particle 2, its neighborhood is particles [1, 3] (with wrap-around)
-    # The best score in this neighborhood is -5 from particle 3.
-    # The actual lbest calculation is more complex with ring topology
-    # Let's test a simple, non-wrapping case first.
-    pso_params['neighborhood_size'] = 1
-    lbest_score = pso._get_local_best_score(2)
-    
-    # Neighborhood for particle 2 is [1, 2, 3]
+    # For particle 2, its neighborhood is particles 1, 2, and 3
     # Scores are [-10, -100, -5]
+    lbest_score = pso._get_local_best_score(2)
     expected_lbest_score = -5
     assert lbest_score == expected_lbest_score
 
@@ -189,11 +219,4 @@ def test_abstract_fitness_function():
             return 1.0
     
     # Can instantiate a concrete subclass
-    assert ConcreteFitness().evaluate(np.array([0, 0])) == 1.0
-
-# --- Test `pso/plotter.py` is not included due to complexity ---
-# Testing the plotter involves checking if a file is created.
-# You could go further and use tools like pytest-mpl to do image
-# comparison, but that is beyond the scope of simple unit tests.
-# The `test_pso_save_results` already confirms that the plotter is
-# being called and creating output files.
+    assert ConcreteFitness().evaluate(np.array([])) == 1.0
